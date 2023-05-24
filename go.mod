097ba234c772be2ebe6c194acee970a47f625f40
Johanchaster22 <dinodarmayanto22@gmailcom>
2023-05-24 23:01:41 +0700

    [FEATURE] rectangle struc

---
+++ go.mod
@@ -0,0 +1,3 @@
+module geometry-lib
+
+go 1.20
---
+++ shape/rectangle.go
@@ -0,0 +1,15 @@
+package shape
+
+type Rectangle struct {
+	Width  float32
+	Length float32
+}
+
+func (rectangle *Rectangle) Area() float32 {
+	return rectangle.Width * rectangle.Length
+}
+
+func (rectangle *Rectangle) Perimeter() float32 {
+	return 2 * (rectangle.Width + rectangle.Length)
+
+}
